05b69123e60772ea42e6532980f783bd39e261a8
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-03-28 21:59:45 -0500

    recorte de proyecto
    no corre si lo pego

--- co.edu.uniquindio.appnuevajdk17fx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.appnuevajdk17fx.appnuevajdk17fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.appnuevajdk17fx.appnuevajdk17fx to javafx.fxml;
-    exports co.edu.uniquindio.appnuevajdk17fx.appnuevajdk17fx;
-}
